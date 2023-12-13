55c5e41ed8ab559bc87e6c8da13cb04b810e68e6
stephaneMalho <stephane.malho@gmail.com>
2023-12-13 13:58:33 +0100

    🚧 create empty Navbar cpnt

---
+++ app/components/shared/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">Navbar</nav>
+  )
+}
+
+export default Navbar
