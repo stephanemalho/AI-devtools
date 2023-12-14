8046a90704feed54cfbd1720ab6bd5b175d48baf
stephaneMalho <stephane.malho@gmail.com>
2023-12-14 12:11:53 +0100

    🚧 add theme button

---
+++ components/shared/navbar/Theme.tsx
@@ -0,0 +1,52 @@
+"use client";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { themes } from "@/constants";
+
+import { useTheme } from "@/context/ThemeProvider";
+import Image from "next/image";
+import React from "react";
+
+const Theme = () => {
+  const { mode, setMode } = useTheme();
+
+  return (
+    <Menubar className="relative border-none bg-transparent shadow-none">
+      <MenubarMenu>
+        <MenubarTrigger className="focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
+          {mode === "light" ? (
+            <Image
+              src="/assets/icons/sun.svg"
+              width={20}
+              height={20}
+              alt="Light Mode"
+              className="active-theme"
+            />
+          ) : (
+            <Image
+              src="/assets/icons/moon.svg"
+              width={20}
+              height={20}
+              alt="Dark Mode"
+              className="active-theme"
+            />
+          )}
+        </MenubarTrigger>
+        <MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300">
+
+       
+          
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  );
+};
+
+export default Theme;
