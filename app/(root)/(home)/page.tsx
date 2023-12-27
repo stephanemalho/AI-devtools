523f0218571d17eb8a77e32892bc4dac1f5d2943
stephaneMalho <stephane.malho@gmail.com>
2023-12-27 11:21:05 +0100

    ✨ add LocalSearchbar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
 
 const Home = () => {
   return (
-    <div>home page</div>
-  )
-}
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">Ask Question</h1>
+        <Link href="/ask-questions/" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient h-[46px] px-4 py-3 !text-light-900">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        /> 
+        
+        Filters
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
