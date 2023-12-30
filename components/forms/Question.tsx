add84fe71efccf16391d6b84923a700a3f5083c8
stephaneMalho <stephane.malho@gmail.com>
2023-12-30 15:29:51 +0100

    ✨ making the form reusable for different form types

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,6 +1,14 @@
 "use client";
+import React, {
+  ReactElement,
+  ReactEventHandler,
+  ReactInstance,
+  useRef,
+  useState,
+} from "react";
+import { Editor } from "@tinymce/tinymce-react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Noop, RefCallBack, useForm } from "react-hook-form";
 import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import {
@@ -14,8 +22,14 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { QuestionsSchema } from "@/lib/validation";
+import { Badge } from "../ui/badge";
+import Image from "next/image";
 
 const Question = () => {
+  const editorRef = useRef(null);
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const type : any = "create";
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
@@ -30,31 +44,83 @@ const Question = () => {
   function onSubmit(values: z.infer<typeof QuestionsSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values);
+    setIsSubmitting(true)
+    
+    try {
+      
+    } catch (error) {
+      
+    } finally {
+      setIsSubmitting(false)
+    }
   }
 
   // 3. Destructure errors from formState
   const { errors } = form.formState;
   const isValid = Object.keys(errors).length === 0;
 
+  const handleInputKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: any
+  ) => {
+    if (e.key === "Enter" && field.name === "tags") {
+      e.preventDefault();
+
+      const tagInput = e.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+
+      if (tagValue !== "") {
+        if (tagValue.length > 15) {
+          return form.setError("tags", {
+            type: "required",
+            message: "Tag must be less than 15 characters.",
+          });
+        }
+      }
+
+      if (!field.value.includes(tagValue as never)) {
+        form.setValue("tags", [...field.value, tagValue]);
+        tagInput.value = "";
+        form.clearErrors("tags");
+      } else {
+        form.trigger();
+      }
+    }
+  };
+
+  const handleTagRemove = (tag: string, field: any) => {
+    const newTags = field.value.filter((t: string) => t !== tag);
+    form.setValue("tags", newTags);
+  };
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex w-full flex-col gap-10">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex w-full flex-col gap-10"
+      >
         <FormField
           control={form.control}
           name="title"
           render={({ field }) => (
             <FormItem className="flex w-full flex-col">
-              <FormLabel className="paragraph-semibold text-dark400_light800">Question title <span className="text-primary-500">*</span></FormLabel>
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Question title <span className="text-primary-500">*</span>
+              </FormLabel>
               <FormControl>
-                <Input className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border" placeholder="...title question" {...field} />
+                <Input
+                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                  placeholder="...title question"
+                  {...field}
+                />
               </FormControl>
               {isValid ? (
                 <FormDescription className="body-regular mt-2.5 text-light-500">
-                  Be specific and imagine you&apos;re asking a question to another person.
+                  Be specific and imagine you&apos;re asking a question to
+                  another person.
                 </FormDescription>
               ) : (
-                <FormMessage className="text-red-500"/>
+                <FormMessage className="text-red-500" />
               )}
             </FormItem>
           )}
@@ -64,17 +130,57 @@ const Question = () => {
           name="explanation"
           render={({ field }) => (
             <FormItem className="flex w-full flex-col gap-3">
-              <FormLabel className="paragraph-semibold text-dark400_light800">Detail explanation of your problem <span className="text-primary-500">*</span></FormLabel>
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Detail explanation of your problem{" "}
+                <span className="text-primary-500">*</span>
+              </FormLabel>
               <FormControl>
-                {/* @todo add an editor component */}
-                <textarea className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[256px] border p-2" placeholder="...write here" {...field} />
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_MCE_EDITOR_API_KEY}
+                  onInit={(evt, editor) =>
+                    // @ts-ignore
+                    (editorRef.current = editor)
+                  }
+                  initialValue=""
+                  init={{
+                    height: 500,
+                    menubar: true,
+                    plugins: [
+                      "advlist",
+                      "autolink",
+                      "lists",
+                      "link",
+                      "image",
+                      "charmap",
+                      "preview",
+                      "anchor",
+                      "searchreplace",
+                      "visualblocks",
+                      "code",
+                      "fullscreen",
+                      "insertdatetime",
+                      "media",
+                      "table",
+                      "codesample",
+                      "help",
+                      "wordcount",
+                    ],
+                    toolbar:
+                      "undo redo | blocks | " +
+                      "codesample | bold italic forecolor | alignleft aligncenter " +
+                      "alignright alignjustify | bullist numlist outdent indent | " +
+                      "removeformat | help",
+                    content_style: "body { font-family:inter; font-size:14px }",
+                  }}
+                />
               </FormControl>
               {isValid ? (
                 <FormDescription className="body-regular mt-2.5 text-light-500">
-                  introduce the problem an expand on what on what you put in the title. Min 20 characters.
+                  introduce the problem an expand on what on what you put in the
+                  title. Min 20 characters.
                 </FormDescription>
               ) : (
-                <FormMessage className="text-red-500"/>
+                <FormMessage className="text-red-500" />
               )}
             </FormItem>
           )}
@@ -84,21 +190,60 @@ const Question = () => {
           name="tags"
           render={({ field }) => (
             <FormItem className="flex w-full flex-col">
-              <FormLabel className="paragraph-semibold text-dark400_light800">Question tags <span className="text-primary-500">*</span></FormLabel>
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Question tags (tape &quot;Enter&quot; to add it{" "}
+                <span className="text-primary-500">*</span>)
+              </FormLabel>
               <FormControl>
-                <Input className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border" placeholder="...tags" {...field} />
+                <>
+                  <Input
+                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                    placeholder="...tags"
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                  />
+                  {isValid ? (
+                    <FormDescription className="body-regular mt-2.5 text-light-500">
+                      Add up to 5 tags to describe what your question is about.
+                    </FormDescription>
+                  ) : (
+                    <FormMessage className="text-red-500" />
+                  )}
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2.5 gap-2.5">
+                      {field.value.map((tag: any) => (
+                        <Badge
+                          key={tag}
+                          className="subtle-medium background-light800_dark300 text-dark400_light500 flex items-center justify-center gap-3 rounded-md border-none px-4 py-2 capitalize"
+                          onClick={() => handleTagRemove(tag, field)}
+                        >
+                          {tag}
+                          <Image
+                            src="/assets/icons/close.svg"
+                            alt="Close icon"
+                            width={12}
+                            height={12}
+                            className="cursor-pointer object-contain invert-0 dark:invert"
+                          />
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
-              {isValid ? (
-                <FormDescription className="body-regular mt-2.5 text-light-500">
-                  Add up to 5 tags to describe what your question is about.
-                </FormDescription>
-              ) : (
-                <FormMessage className="text-red-500"/>
-              )}
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <Button
+          type="submit"
+          className="primary-gradient w-fit !text-light-900"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? (
+            <>{type === "edit" ? "Editing..." : "Posting..."}</>
+          ) : (
+            <>{type === "edit" ? "Edit Question" : "Ask a Question"}</>
+          )}
+        </Button>
       </form>
     </Form>
   );
