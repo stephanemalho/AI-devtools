b2d06103afb40baf405178b5924b41f0eba71c91
stephaneMalho <stephane.malho@gmail.com>
2024-01-01 13:58:38 +0100

    ✨ starting with mongoose config

---
+++ lib/actions/question.action.ts
@@ -0,0 +1,11 @@
+"use server"
+
+export async function createQuestion(params : any) {
+    console.log("createQuestion", params);
+  try {
+    
+  } catch (error) {
+    
+  }
+  
+}
---
+++ lib/mongoose.ts
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+
+let isConnected: boolean = false;
+
+export const connectToDatabase = async () => {
+  mongoose.set('strictQuery', true);
+};
