a2fa29eab5572543b2ea146434672c09299c57ad
stephaneMalho <stephane.malho@gmail.com>
2024-03-05 13:19:35 +0100

    🐛 fix eslint errors to deploy

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -60,9 +60,9 @@ export async function deleteUser(params : DeleteUserParams) {
     }
 
     // get user question ids
-    const userQuestionIds = await Question.find({
-      author: user._id,
-    }).distinct("_id");
+    // const userQuestionIds = await Question.find({
+    //   author: user._id,
+    // }).distinct("_id");
 
     await Question.deleteMany({
     author: user._id,});
